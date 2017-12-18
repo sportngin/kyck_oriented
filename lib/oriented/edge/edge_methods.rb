f56bde93b14223a5c564a35c7f1e905892333f48
Nick Sieger <nick@nicksieger.com>
2017-12-18 15:01:46 -0600

    whitespace and dead code

--- lib/oriented/edge/edge_methods.rb
+++ lib/oriented/edge/edge_methods.rb
@@ -1,20 +1,8 @@
 module Oriented
   module EdgeMethods
-    extend ActiveSupport::Concern   
+    extend ActiveSupport::Concern
 
-    module ClassMethods 
-
-      def new(*args)
-        validate_constructor_args(*args)
-        start_vertex, end_vertex, label, props = *args
-        wrapper = super()
-        java_obj = Oriented::Core::JavaEdge.new(start_vertex.__java_obj, end_vertex.__java_obj, label)        
-        wrapper.__java_obj = java_obj
-
-        wrapper.write_default_values
-        props.each_pair {|attr,val| wrapper.public_send("#{attr}=", val)} if props       
-        wrapper
-      end
+    module ClassMethods
 
       def find(id)
         edge = Oriented.graph.get_edge(id)
@@ -32,8 +20,8 @@ module Oriented
           return edge if edge.class == Oriented::Core::JavaEdge
           edge.kind_of?(self) ? edge : nil
       end
-      
-      def to_adapter 
+
+      def to_adapter
          self
       end
 
@@ -47,7 +35,7 @@ module Oriented
         raise ArgumentError.new "End vertex not supplied" unless end_vertex
         raise ArgumentError.new "End vertex is not a vertex" unless end_vertex.is_a?(Vertex)
       end
-      
+
     end
   end
 end
